8ae84f68f46d421f8db1f9f6cf0535a542b1e0cb
fridokus <oskarfr@student.chalmers.se>
2016-11-16 15:36:59 +0100

    Added a function for simple plot of individuals

---
+++ PlotIndividualsSimple.m
@@ -0,0 +1,16 @@
+function [] = PlotIndividualsSimple(individuals)
+%PLOTINDIVIDUALSSIMPLE Plot individuals using colors
+%
+%   Inputs:
+%       Individuals
+%
+%   Outputs:
+%       Plot
+
+
+    pointsize = 8;
+    scatter(individuals(1,:), individuals(2,:), ...
+        pointsize, individuals(4,:), 'filled');
+
+end
+
