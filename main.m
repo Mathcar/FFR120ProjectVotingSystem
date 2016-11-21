55427a9ed9a65c49abceb540f841c3e2876fbdc9
HildaZz <hildasa@student.chalmers.se>
2016-11-21 14:40:56 +0100

    fixade index

--- main.m
+++ main.m
@@ -1,28 +1,28 @@
 %-------------------FFR120, Voting system, Main--------------------------%
 n = 1e3; 
 nCities =10; 
-citySize = 1; 
+citySize = 5; 
 proportionAffected = 0.01;
-time = 0; 
 nTimeSteps = 1e3;  
+time = 0; 
 
 [individuals] = GenerateIndividuals(n, nCities, citySize);
 
 PlotIndividualsSimple(individuals);
-
 
 while( time < nTimeSteps)
     
     time = time + 1;
     
     [ selectedIndex1, selectedIndex2 ] = Selection(individuals);
-    individual1 = individuals(selectedIndex1,:);
-    individual2 =  individuals(selectedIndex2,:);
+    individual1 = individuals(:,selectedIndex1);
+    individual2 =  individuals(:,selectedIndex2);
     
     [ newOpinion1, newOpinion2 ] = OpinionTransfer( individual1, individual2 );
-    individuals(selectedIndex1,4)= newOpinion1;
-    individuals(selectedIndex2,4)= newOpinion2;
+    individuals(4,selectedIndex1)= newOpinion1;
+    individuals(4,selectedIndex2)= newOpinion2;
     
     individuals = Media(individuals, proportionAffected);
     
 end
+
