8cbde9493270c34711c261dbe356f11cb5b96f98
Mathcar <mattec_93@hotmail.com>
2016-11-21 11:44:54 +0100

    implemented OpinionTransfer

--- OpinionTransfer.m
+++ OpinionTransfer.m
@@ -1,6 +1,13 @@
 function [ newOpinion1, newOpinion2 ] = OpinionTransfer( induvidual1, induvidual2 )
     
+    opinion1 = induvidual1(4);
+    opinion2 = induvidual2(4);
     
+    carisma1 = induvidual1(3);
+    carisma2 = induvidual2(3);
+    
+    newOpinion1 = opinion1 + carisma2*(opinion2-opinion1);
+    newOpinion2 = opinion2 + carisma1*(opinion1-opinion2);
     
 end
 
