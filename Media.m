5bd926d51ef56c7a542b0a0d309a421048e704ab
fridokus <oskarfr@student.chalmers.se>
2016-11-21 13:23:03 +0100

    Added Media. Changed OpinionTransfer a bit, so that 2 people interacting can't go past each other on opinion.

---
+++ Media.m
@@ -0,0 +1,36 @@
+function individuals = Media(individuals, proportionAffected)
+% MEDIA Media affecting some percentage of all individuals
+%
+%   Inputs:
+%       individuals: All invividuals
+%       proportionAffected: The proportion of people which will be affected by
+%       the media.
+%
+%   Outputs:
+%       individuals: All individuals
+    
+    mediaEffectScalar = .2;
+
+    n = size(ind, 2);
+    nAffected = floor(proportionAffected*n);
+    indicesAffected = randperm(n, nAffected);
+    
+    mediaPosition = randi(2)*2 - 3; % media pos either -1 or 1
+    mediaEffect = mediaEffectScalar*rand;
+    
+    for i = 1:nAffected
+        
+        newOpinion = individuals(4, indicesAffected(i)) + ...
+            mediaPosition*mediaEffect;
+        
+        individuals(4, indicesAffected(i)) = newOpinion - ...
+            (newOpinion<0)*newOpinion - (newOpinion>1)*(newOpinion-1);
+        
+        % newOpinion might become <0 or >1, so it is corrected into the
+        % nearest point on [0, 1].
+        
+    end
+    
+
+end
+
