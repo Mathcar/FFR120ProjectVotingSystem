72640b5837a0edb207bfd8400c43c3ee763e4b52
fridokus <oskarfr@student.chalmers.se>
2016-11-21 13:39:41 +0100

    typ inget

--- OpinionTransfer.m
+++ OpinionTransfer.m
@@ -10,5 +10,6 @@ function [ newOpinion1, newOpinion2 ] = OpinionTransfer( individual1, individual
     newOpinion2 = opinion2 + (charisma1/(charisma2 + charisma1))*(opinion1-opinion2);
     
 end % Tänker att kanske opinion ändras för mycket här? att vi borde ha en 
-    % faktor ~.2 framför andra termen i newOpinion.
+    % faktor ~.2 framför andra termen i newOpinion. Just nu möts de på
+    % mitten
 
