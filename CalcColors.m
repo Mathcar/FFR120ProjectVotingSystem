0742ad8913b456c581a6bef4ebc1161de9793d0e
Steve Jobs <stevejobs@pizzapal.local>
2016-11-22 19:20:31 +0100

    had indices being one too large sometimes. Now stays within range

--- CalcColors.m
+++ CalcColors.m
@@ -1,7 +1,8 @@
 function colors = CalcColors(partiesList)
-   nColors = size(colormap, 1);
-   baseColors = colormap;
-   colorIndices = floor(nColors*partiesList*(1-eps)) + 1;
-   colors = baseColors(colorIndices, :);
-   
+    nColors = size(colormap, 1);
+    baseColors = colormap;
+    
+    colorIndices = floor((nColors-1)*partiesList) + 1;
+    
+    colors = baseColors(colorIndices, :);
 end
