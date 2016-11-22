850a077c13d2e16dbf13134319221ae930f83158
fridokus <oskarfr@student.chalmers.se>
2016-11-22 15:29:23 +0100

    Added Parties

---
+++ Parties.m
@@ -0,0 +1,30 @@
+function partiesList = Parties(nParties, minDistance)
+%PARTIES Gives a list of parties
+%
+%   Inputs:
+%       nParties: number of parties
+%       minDistance: minimum distance between parties
+%
+%   Output:
+
+    if (minDistance*nParties > .95)
+        minDistance = .95/nParties;
+    end
+    
+    done = 0;
+    
+    while not(done)
+        
+        partiesList = sort(rand(nParties, 1));
+        done = 1;
+        
+        for i = 2:nParties
+            if partiesList(i) - partiesList(i-1) < minDistance
+                done = 0;
+            end
+        end
+    
+    end
+    
+end
+
