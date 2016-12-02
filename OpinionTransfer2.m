6050a04eefa80709ef98e05f2450992115fa2273
fridokus <oskarfr@student.chalmers.se>
2016-12-02 16:06:20 +0100

    Mapping into [0,1] in OpinionTransfer2 now looks better

--- OpinionTransfer2.m
+++ OpinionTransfer2.m
@@ -17,22 +17,14 @@ function [ newOpinion1, newOpinion2, newCertainty1, newCertainty2 ] = ...
     certainty1 = individual1(4);
     certainty2 = individual2(4);
     
-
     newCertainty1 = certainty1 + threshold - abs(opinion1-opinion2);
     newCertainty2 = certainty2 + threshold - abs(opinion1-opinion2);
     
-    if newCertainty1 > 1
-        newCertainty1 = 1;
-    elseif newCertainty1 < 0
-        newCertainty1 = 0;
-    end
-    
-     if newCertainty2 > 1
-        newCertainty2 = 1;
-    elseif newCertainty2 < 0
-        newCertainty2 = 0;
-    end
-    
+    newCertainty1 = newCertainty1 - (newCertainty1<0)*newCertainty1 ...
+        - (newCertainty1>1)*(newCertainty1-1);
+    newCertainty2 = newCertainty2 - (newCertainty2<0)*newCertainty2 ...
+        - (newCertainty2>1)*(newCertainty2-1);
+        
     newOpinion1 = opinion1 + transferEffect*...
         (certainty2/(certainty2 + certainty1+eps))*(opinion2-opinion1);
     newOpinion2 = opinion2 + transferEffect*...
