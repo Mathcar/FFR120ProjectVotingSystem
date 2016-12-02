577de28eb1b965ac145216abd24c633415e1aa37
Steve Jobs <stevejobs@pizzapal.local>
2016-12-02 18:01:54 +0100

    count votes optimized. wen from 18.953 s to 4.187 s for 100000 calls

--- CountVotes.m
+++ CountVotes.m
@@ -1,8 +1,3 @@
 function counts = CountVotes(individuals, partiesList)
-     nParties = length(partiesList);
-     bins = 1:nParties;
-     opinions = individuals(3,:);
-     distances = pdist2(opinions', partiesList);
-     [~, votes] = min(distances,[], 2);
-     [counts, ~] = hist(votes, bins);
+     [counts, ~] = hist(individuals(3,:), partiesList);
 end
